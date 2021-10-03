36ead2cd355e38ab3219761f29bd92de374866fd
Abhay Pawar <54652556+abhaypawar@users.noreply.github.com>
2021-10-03 21:33:13 +0530

    Create Number_Triangle.py

---
+++ Set 2/Problem 5/Number_Triangle.py
@@ -0,0 +1,9 @@
+def number_triangle(n):
+    num = 1
+    for i in range(0, n):
+        for j in range(0, i+1):
+            print(num, end=" ")
+            num = num + 1
+        print("\r")
+n = 2
+number_triangle(n)
